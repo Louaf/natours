7a4de7a9bf4b166bd5679a27566eb7b25edd78b8
Louaf <albaqaryyousif@gmail.com>
2025-12-14 08:05:18 +0200

    handlerFactory updateOne

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -18,5 +18,8 @@ router
   )
   .get(reviewController.getAllReviews);
 
-router.route('/:id').delete(reviewController.deleteReview);
+router
+  .route('/:id')
+  .delete(reviewController.deleteReview)
+  .patch(reviewController.updateReview);
 module.exports = router;
