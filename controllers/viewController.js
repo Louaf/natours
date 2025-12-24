296dc25112066c687213398252590768a42aba8b
Louaf <albaqaryyousif@gmail.com>
2025-12-24 10:51:48 +0200

    learning SSR with pug : filling tour page templates with data part1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) get tours data
   const tours = await Tour.find();
@@ -9,8 +10,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     tours,
   });
 });
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    tours,
+exports.getTour = catchAsync(async (req, res) => {
+  //console.log(req.params.slug);
+  //1) get the data for the requested tour (including the )
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review ratings user',
   });
-};
+  //console.log(tours);
+  //2)buld the template
+
+  // render template using the data
+  res.status(200).render('tour', {
+    tour,
+  });
+});
